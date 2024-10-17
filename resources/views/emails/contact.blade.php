aa57c8be0bd939acad8a31562eedcf33e003b430
MarchioriRH <rubenmarchiori@gmail.com>
2024-10-17 20:53:41 -0300

    Perf: contact msg view

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,8 @@
+<p>Ha recibido un nuevo mensaje de contacto de:</p>
+
+<p><strong>Nombre:</strong> {{ $name }}</p>
+<p><strong>Telefono:</strong> {{ $phone }}</p>
+<p><strong>Email:</strong> {{ $email }}</p>
+<p><strong>Asunto:</strong> {{ $subject }}</p>
+<p><strong>Mensaje:</strong></p>
+<p>{{ $message }}</p>
