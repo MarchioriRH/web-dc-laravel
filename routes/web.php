0f622794af8eac3ac087615ff1e064f18d1b76a8
Ruben Horacio Marchiori <rubenmarchiori@gmail.com>
2025-05-27 14:34:29 -0300

    Feat: add breeze, bootstrap and npm

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DesktopController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\NewsController;
-use App\Http\Controllers\DownloadController;
 
-// Route::get('/home', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/home', [DesktopController::class, 'index']);
-// Route::get('/contact', [ContactController::class, 'showForm'])->name('contact.show');
-Route::post('/contact', [ContactController::class, 'submitForm'])->name('contact.submit');
-Route::get('/descargas', [DownloadController::class, 'index'])->name('download.index');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/hidden/add-news', [NewsController::class, 'create'])->name('news.create');
-Route::post('/hidden/add-news', [NewsController::class, 'store'])->name('news.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
